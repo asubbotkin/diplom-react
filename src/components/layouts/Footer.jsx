ab3170c3d1c023b9c9a0161f8a46afc57cd69bd6
Andrew Subbotkin <asubbotkin@gmail.com>
2024-03-13 15:48:51 +0200

    Modified footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -9,63 +9,67 @@ function Footer() {
   return (
     <div className={styles.footer}>
       <div className="container">
-        <Row className="d-flex align-items-center">
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <NavLink to="." end>
-              <img src={logo} alt="Logo" />
-            </NavLink>
-            <ul className="policy text-center text-sm-start">
-              <li className={styles.policyItem}>© Все права защищены</li>
-              <li className={styles.policyItem}>Политика конфиденциальности</li>
-              <li className={styles.policyItem}>Публичная оферта</li>
-            </ul>
-          </Col>
-          <Col sm={4} md={5} lg={3} xl={4}>
-            <nav className={menuStyles.mainMenu}>
-              <NavLink className={menuStyles.mainMenuItem} to="." end>
-                Главная
+        <Row>
+          <div className={styles.footerContentWrapper}>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <NavLink to="." end>
+                <img src={logo} alt="Logo" />
               </NavLink>
-              <NavLink className={menuStyles.mainMenuItem} to="shop">
-                Магазин
-              </NavLink>
-              <NavLink className={menuStyles.mainMenuItem} to="about">
-                О бренде
-              </NavLink>
-              <NavLink className={menuStyles.mainMenuItem} to="contacts">
-                Контакты
-              </NavLink>
-            </nav>
-          </Col>
-          <Col>
-            <div className={styles.socialsWrapper}>
-              <Link className="rightMenuItem" href="tel:+74958235412">
-                +7 (495) 823-54-12
-              </Link>
-              <Link href="#" className="rightMenuItem woomail">
-                hello@womazing.com
-              </Link>
-              <div className="socials d-inline-flex">
-                <Link
-                  className="socials socialsLink socialsInst"
-                  href="https://www.instagram.com"
-                  rel="nofollow noopener noreferrer"
-                  target="_blank"></Link>
-                <Link
-                  className="socials socialsLink socialsFb"
-                  href="https://www.facebook.com"
-                  rel="nofollow noopener noreferrer"
-                  target="_blank"></Link>
-                <Link
-                  className="socials socialsLink socialsTwit"
-                  href="https://www.twitter.com"
-                  rel="nofollow noopener noreferrer"
-                  target="_blank"></Link>
+              <ul className="policy text-center text-sm-start">
+                <li className={styles.policyItem}>© Все права защищены</li>
+                <li className={styles.policyItem}>
+                  Политика конфиденциальности
+                </li>
+                <li className={styles.policyItem}>Публичная оферта</li>
+              </ul>
+            </Col>
+            <Col sm={4} md={5} lg={3} xl={4}>
+              <nav className={menuStyles.mainMenu}>
+                <NavLink className={menuStyles.mainMenuItem} to="." end>
+                  Главная
+                </NavLink>
+                <NavLink className={menuStyles.mainMenuItem} to="shop">
+                  Магазин
+                </NavLink>
+                <NavLink className={menuStyles.mainMenuItem} to="about">
+                  О бренде
+                </NavLink>
+                <NavLink className={menuStyles.mainMenuItem} to="contacts">
+                  Контакты
+                </NavLink>
+              </nav>
+            </Col>
+            <Col>
+              <div className={styles.socialsWrapper}>
+                <Link className="rightMenuItem" href="tel:+74958235412">
+                  +7 (495) 823-54-12
+                </Link>
+                <Link href="#" className="rightMenuItem woomail">
+                  hello@womazing.com
+                </Link>
+                <div className="socials d-inline-flex">
+                  <Link
+                    className="socials socialsLink socialsInst"
+                    href="https://www.instagram.com"
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"></Link>
+                  <Link
+                    className="socials socialsLink socialsFb"
+                    href="https://www.facebook.com"
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"></Link>
+                  <Link
+                    className="socials socialsLink socialsTwit"
+                    href="https://www.twitter.com"
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"></Link>
+                </div>
+                <div className="cadrds">
+                  <img src="../../img/cards.png" alt="" />
+                </div>
               </div>
-              <div className="cadrds">
-                <img src="../../img/cards.png" alt="" />
-              </div>
-            </div>
-          </Col>
+            </Col>
+          </div>
         </Row>
       </div>
     </div>
